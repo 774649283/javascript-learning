ff58dae525bf7a930f927e91f7c912ca5b86914f
NoJsJa <yangwei13@outlook.com>
2018-11-05 01:12:48 +0800

    update: animation.js

--- js-animation/animation.js
+++ js-animation/animation.js
@@ -1,3 +1,4 @@
+// 动画帧请求函数兼容写法
 window.requestAnimationFrame = (function(){
   return  window.requestAnimationFrame       ||
           window.webkitRequestAnimationFrame ||
@@ -9,37 +10,35 @@ window.requestAnimationFrame = (function(){
           };
 })();
 
+/**
+ * [tween 缓动算法]
+ * @param {[Number]}  t [动画已经消耗的时间]
+ * @param {[String]}  b [目标开始的位置]
+ * @param {[String]}  c [目标开始位置和结束位置的距离]
+ * @param {[Number]}  d [动画总持续时间]
+ */
+var tween = {
+  linear: function( time, start, distance, duration ) { return distance*time/duration + start; },
+  easeIn: function( time, start, distance, duration ) { return distance * ( time /= duration ) * time + start; },
+  strongEaseIn: function(time, start, distance, duration) { return distance * ( time /= duration ) * time * time * time * time + start; },
+  strongEaseOut: function(time, start, distance, duration) { return distance * ( ( time = time / duration - 1) * time * time * time * time + 1 ) + start; },
+  sinEaseIn: function( time, start, distance, duration ) { return distance * ( time /= duration) * time * time + start; },
+  sinEaseOut: function(time,start,distance,duration){ return distance * ( ( time = time / duration - 1) * time * time + 1 ) + start; },
+};
 
+
+/* ------------------- 动画控制类 ------------------- */
 var Animation = function () {
-  this.queue = []; // animation queue
-  /**
-    * status
-    -- pending => animation ready
-    -- running => animation running
-    -- pause   => animation pause
-    -- stop    => animation stop
-  */
   this.store = { // status store
-    /*
-      [element-selector]: {
-        selector: [element-selector],
-        element: document.querySelector(this.select),
-        status: 'pending' || 'running' || 'pause' || 'stop',
-        func: 'linear' || 'easeIn' || 'easeOut',
-        positionX: 'xxx',
-        positionY: 'xxx',
-      }
-    */
   };
 };
 
-/* ------------------- 设置目标 ------------------- */
-Animation.prototype.setTarget = function (selector, func) {
+/* ------------------- 初始化处理元素 ------------------- */
+Animation.prototype.setTarget = function (selector) {
   var element = document.querySelector(selector);
-  if (element && !this.store[selector]) {
-    element.style.left = element.style.left || 0;
-    element.style.top = element.style.top || 0;
-    element.style.position = 'relative';
+
+  if (element) {
+    // element.style.position = 'relative';
     this.store[selector] = {
       selector: selector,
       element: document.querySelector(selector),
@@ -50,84 +49,138 @@ Animation.prototype.setTarget = function (selector, func) {
           x: 'xxx', // 相对位置
           y: 'xxx', // 相对位置
           duration: 'xxx', // 毫秒
+          func: func,
         }
         */
       ],
-      func: func || 'linear',
-      positionX: 0, // 一个阶段动画的相对位置X
-      positionY: 0, // 一个阶段动画的相对位置Y
+      timeStart: '',
+      positionStart: {
+        x: '',
+        y: '',
+      },
+      positionEnd: {
+        x: '',
+        y: '',
+      },
     };
   }
 };
 
-/* ------------------- 开始动画 ------------------- */
-Animation.prototype.start = function (selector) {
+/**
+ * [start 开始动画]
+ * @param  {[String]} selector [选择器]
+ * @param  {[type]} func     [缓动动画]
+ */
+Animation.prototype.start = function (selector, func) {
+  var that = this;
+  var target = this.store[selector];
+  target.status = 'running';
+
+  // 帧调用函数
+  that.update({x: 0, y: 0}, selector);
+};
+
+/**
+ * [update 更新位置]
+ * @param  {[type]} selector [description]
+ */
+Animation.prototype.update =  function (position, selector) {
   var target = this.store[selector],
     that = this,
-    height, width
-    abs = target.queue[0]['x'] / Math.abs(target.queue[0]['x']),
-    timeStart = new Date().getTime(),
-    timeNow = timeStart;
+    timeUsed,
+    positionX, positionY;
+  //
+  if (!target || !target.queue.length) {
+    target.status = 'pending';
+    return;
+  };
 
-  if (!target) return;
+  // reset position
+  target.element.style.left = position.x + 'px';
+  target.element.style.top = position.y + 'px';
 
-  target.status = 'running';
+  // position
+  target.positionStart = {
+    x: position.x,
+    y: position.y,
+  };
+  target.positionEnd = {
+    x: position.x + target.queue[0].x,
+    y: position.y + target.queue[0].y,
+  };
+  // time
+  target.timeStart = new Date().getTime();
+
+  // 递归调用
   var callback = function () {
-    if (target.status === 'pause' || target.status === 'stop') {
+    timeUsed = new Date().getTime() - target.timeStart;
+    // 当前动画完成
+    if (timeUsed >= target.queue[0].duration) {
+      target.queue.shift();
+      that.step(target.element, target.positionEnd.x, target.positionEnd.y);
+      target.status = 'running';
+      // var position = target.element.getBoundingClientRect();
+      var position = {
+        x: parseInt(target.element.style.left),
+        y: parseInt(target.element.style.top),
+      };
+      // 下一个动画
+      that.update(position, selector);
       return;
     }
+    positionX = target.queue[0].func(
+      timeUsed,
+      target.positionStart.x,
+      target.positionEnd.x - target.positionStart.x,
+      target.queue[0].duration,
+    );
+    positionY = target.queue[0].func(
+      timeUsed,
+      target.positionStart.y,
+      target.positionEnd.y - target.positionStart.y,
+      target.queue[0].duration,
+    );
+    that.step(target.element, positionX, positionY);
 
-    // timeNow = new Date().getTime();
-    height = (target.queue[0]['y'] - that.store[selector].positionY);
-    width = (target.queue[0]['x'] - that.store[selector].positionX);
-    that.step( selector, 2 * abs, 2 * (height / width) * abs );
-
-    if (Math.abs(target.positionX) >= Math.abs(target.queue[0]['x'])) {
-      target.element.style.left = (target.queue[0]['x'] - target.positionX) + parseInt(target.element.style.left)  + 'px'; // restore
-      target.element.style.top = (target.queue[0]['y'] - target.positionY) + parseInt(target.element.style.top)  + 'px'; // restore
-      target.queue.shift();
-      target.positionX = 0;
-      target.positionY = 0;
-      // timeNow = new Date().getTime();
-      if (!target.queue[0]) {
-        target.status = 'pending';
-      } else {
-        target.status = 'running';
-        abs = target.queue[0]['x'] / Math.abs(target.queue[0]['x']);
-        requestAnimationFrame(callback);
-      }
-    } else {
-      requestAnimationFrame(callback);
-    }
+    requestAnimationFrame(callback);
   };
 
   requestAnimationFrame(callback);
 };
 
+/**
+ * [step dom操作]
+ * @param  {[DOM]} element [dom 元素]
+ * @param  {[Number]} x        [x坐标]
+ * @param  {[Number]} y        [y坐标]
+ */
+Animation.prototype.step = function (element, x, y) {
+  element.style.left = x + 'px';
+  element.style.top = y + 'px';
+};
+
 /* ------------------- 暂停动画 ------------------- */
 Animation.prototype.pause = function (selector) {
-  if (this.store[selector]) {
-    this.store[selector].status = 'pause';
-  }
+
 };
 
 /* ------------------- 结束动画 ------------------- */
 Animation.prototype.stop = function (selector) {
-  if (this.store[selector]) {
-    this.store[selector].status = 'stop';
-    this.store[selector].positionX = 0;
-    this.store[selector].positionY = 0;
-    this.clear(selector);
-  }
+
 };
 
-/* ------------------- 添加动画队列 ------------------- */
+/**
+ * [push 加入动画队列]
+ * @param  {[String]} selector [dom选择器]
+ * @param  {[Object]} conf     [位置数据]
+ */
 Animation.prototype.push = function (selector, conf) {
   if (this.store[selector]) {
     this.store[selector].queue.push({
       x: conf.x,
       y: conf.y,
       duration: conf.duration || 1000,
+      func: tween[conf.func] || tween['linear'],
     });
   }
 };
@@ -142,16 +195,6 @@ Animation.prototype.pop = function (selector) {
 /* ------------------- 清空动画队列 ------------------- */
 Animation.prototype.clear = function (selector) {
   if (this.store[selector]) {
-    this.store[selector].queue.length = 0;
+    this.store[selector].queue.length = 1;
   }
-};
-
-/* ------------------- 动画步进 ------------------- */
-Animation.prototype.step = function (selector, x, y) {
-  var target = this.store[selector];
-  console.log(target.element.offsetLeft, target.element.style.left);
-  target.element.style.left = x + parseInt(target.element.style.left) + 'px';
-  target.element.style.top = y + parseInt(target.element.style.top) + 'px';
-  target.positionX = target.positionX + x;
-  target.positionY = target.positionY + y;
 };
